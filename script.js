877f20478c3ae6942858edcd7e2905043d5f141d
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-12-11 11:32:32 +0100

    add odin image and show password eye icon to password input

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const passwordInput = document.getElementById("password");
-//     const confirmPasswordInput = document.getElementById("confirm-password");
-//     const errorMessage = document.querySelector(".form-info span");
-
-//     confirmPasswordInput.addEventListener("input", function () {
-//       const passwordValue = passwordInput.value;
-//       const confirmPasswordValue = confirmPasswordInput.value;
-
-//       if (passwordValue === confirmPasswordValue) {
-//         errorMessage.style.display = "none";
-//       } else {
-//         errorMessage.style.display = "block";
-//       }
-//     });
-//   });
-
 let passwordInput = document.getElementById("password");
 let confirmPasswordInput = document.getElementById("confirm-password");
 let errorMessage = document.querySelector(".form-info span");
@@ -29,3 +12,15 @@ confirmPasswordInput.addEventListener("input", function () {
     errorMessage.style.display = "block";
   }
 });
+
+function togglePasswordVisibility() {
+  let passwordInput = document.getElementById("password");
+  let togglePasswordIcon = document.querySelector(".toggle-password-icon");
+
+  // Toggle the password field type between "password" and "text"
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+
+  // Change the eye icon based on the password visibility state
+  togglePasswordIcon.textContent =
+    passwordInput.type === "password" ? "👁️" : "👁️";
+}
